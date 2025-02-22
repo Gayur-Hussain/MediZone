f2605b0a9b3f6fc6ec8ab2dd8a973175c185c208
Gayur Hussain <gayurhussain1@gmail.com>
2025-02-23 02:31:11 +0530

    fix import issue

--- components/navbar/NavigationBarWrapper.js
+++ components/navbar/NavigationBarWrapper.js
@@ -5,7 +5,6 @@ import { auth } from "@clerk/nextjs/server";
 export default async function NavigationBarWrapper() {
 	const user = await auth();
 
-	// If the user is logged in, fetch their details
 	let userDetails = null;
 	if (user) {
 		userDetails = await fetchUserDetailsAction(user.userId);
