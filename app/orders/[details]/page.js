a677ef40177bade22deeff253d556f4af384e758
Gayur Hussain <gayurhussain1@gmail.com>
2025-03-20 21:56:07 +0530

    debugging issue

--- app/orders/[details]/page.js
+++ app/orders/[details]/page.js
@@ -5,7 +5,7 @@ import React from "react";
 const OrderDetails = async ({ params }) => {
 	const { details: orderId } = await params;
 	const response = await fetchSpecificUserOrder(orderId);
-	// console.log(response?.data);
+	console.log(response?.data);
 	return (
 		<div className="px-4 mt-24 lg:px-28">
 			<h1>Params {orderId}</h1>
