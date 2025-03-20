d4995f602583d53167150e128d984bec4e606085
Gayur Hussain <gayurhussain1@gmail.com>
2025-03-20 22:17:34 +0530

    Fix issue of not showing the user order details on the deployed version

--- app/orders/[details]/page.js
+++ app/orders/[details]/page.js
@@ -1,14 +1,18 @@
 import { fetchSpecificUserOrder } from "@/actions/userOrders";
 import SpecificOrderDetails from "@/components/orderPage/SpecificOrderDetails";
-import React from "react";
 
 const OrderDetails = async ({ params }) => {
-	const { details: orderId } = await params;
+	// Destructure the orderId from params.details
+	const { details: orderId } = params;
+
+	// Fetch order data using the orderId
 	const response = await fetchSpecificUserOrder(orderId);
+
+	// Optionally log the response data to check if it's correct
 	console.log(response?.data);
+
 	return (
 		<div className="px-4 mt-24 lg:px-28">
-			<h1>Params {orderId}</h1>
 			<SpecificOrderDetails order={response?.data} />
 		</div>
 	);
